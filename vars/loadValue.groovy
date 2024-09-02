1793fe3d9560ea7fe8d2ca1130cbf9b7cd0751f3
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-02 21:12:57 +0530

    Update loadValue.groovy

--- vars/loadValue.groovy
+++ vars/loadValue.groovy
@@ -1,6 +1,6 @@
 def call() {
   def add_content = libraryResource "add.sh"
   def mul_content = libraryResource "mul.sh"
-  writeFile file: add_script.sh, text: add_content
-  writeFile file: mul_script.sh, text: mul_content
+  writeFile file: add.sh, text: add_content
+  writeFile file: mul.sh, text: mul_content
 }
