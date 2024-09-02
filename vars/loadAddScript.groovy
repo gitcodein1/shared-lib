d51b27da1fb8faffcd318c761ab76ede231b00ff
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-03 01:08:01 +0530

    Update loadAddScript.groovy

--- vars/loadAddScript.groovy
+++ vars/loadAddScript.groovy
@@ -2,7 +2,3 @@ def foradd(addscript) {
   def add_content = libraryResource "add/add.sh"
   writeFile file: addscript, text: add_content
 }
-def formul(mulscript) {
-  def mul_content = libraryResource "mul/mul.sh"
-  writeFile file: mulscript, text: mull_content
-}
