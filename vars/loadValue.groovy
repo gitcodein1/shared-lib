6bcf690e604b9b87f357265dc0fdc379a19f9e23
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-02 23:34:48 +0530

    Update loadValue.groovy

--- vars/loadValue.groovy
+++ vars/loadValue.groovy
@@ -1,9 +1,9 @@
-def foradd() {
+def foradd(String) {
   def add_content = libraryResource "add.sh"
   writeFile file: add.sh, text: add_content
   sh "cat add.sh"
 }
-def formul() {
+def formul(String) {
   def mul_content = libraryResource "mul.sh"
   writeFile file: mul.sh, text: mul_content
   sh "cat mul.sh"
