cd8d5d8ceb6072158b4c3e2eb7563e131919c555
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-03 01:15:28 +0530

    Update cal.groovy

--- vars/cal.groovy
+++ vars/cal.groovy
@@ -1,8 +1,10 @@
 def add(value1,value2) {
   loadAddScript("addscript")
+  sh "cat addscript"
   sh "bash addscript ${value1} ${value2}"
 }
 def mul(value1,value2) {
   loadMulScript("mulscript")
+  sh "cat mulscript"
   sh "bash mulscript ${value1} ${value2}"
 }
