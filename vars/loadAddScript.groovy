f69c1058aab43bca57cc787b1f045e26971b84a5
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-03 00:00:00 +0530

    Update loadAddScript.groovy

--- vars/loadAddScript.groovy
+++ vars/loadAddScript.groovy
@@ -1,5 +1,4 @@
-def call() {
-  def add_content = libraryResource "add.sh"
-  writeFile file: add.sh, text: add_content
-  sh "bash add.sh"
+def call(String script) {
+  def add_content = libraryResource "add/${script}"
+  writeFile file: script, text: add_content
 }
