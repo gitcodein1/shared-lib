08733352a0d04de183c392f33e2d4885c6e999c2
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-04 12:28:25 +0530

    Update loadFileCheck.groovy

--- vars/loadFileCheck.groovy
+++ vars/loadFileCheck.groovy
@@ -1,5 +1,5 @@
 def call(check_script) {
   def content = libraryResource "check_file.sh"
   writeFile file: check_script, text: content
-  sh "chmod +x {check_script}"
+  sh "chmod +x ${check_script}"
 }
